777084684b93416734e5d88b67fcdced7a8bac3f
lex <luohai2233@163.com>
2021-12-04 10:26:21 +0800

    初始化数据库时，用户头像链接更新

--- source/admin.go
+++ source/admin.go
@@ -13,8 +13,8 @@ var Admin = new(admin)
 type admin struct{}
 
 var admins = []model.SysUser{
-	{MODEL: global.MODEL{ID: 1, CreatedAt: time.Now(), UpdatedAt: time.Now()}, UUID: uuid.NewV4().String(), Username: "admin", Password: "e10adc3949ba59abbe56e057f20f883e", NickName: "超级管理员", HeaderImg: "http://xxx/images/avatar.jpeg", AuthorityId: "100"},
-	{MODEL: global.MODEL{ID: 2, CreatedAt: time.Now(), UpdatedAt: time.Now()}, UUID: uuid.NewV4().String(), Username: "superUser", Password: "e10adc3949ba59abbe56e057f20f883e", NickName: "普通管理员", HeaderImg: "http://xxx/images/avatar.jpeg", AuthorityId: "1001"},
+	{MODEL: global.MODEL{ID: 1, CreatedAt: time.Now(), UpdatedAt: time.Now()}, UUID: uuid.NewV4().String(), Username: "admin", Password: "e10adc3949ba59abbe56e057f20f883e", NickName: "超级管理员", HeaderImg: "https://www.mineor.xyz/images/avatar.png", AuthorityId: "100"},
+	{MODEL: global.MODEL{ID: 2, CreatedAt: time.Now(), UpdatedAt: time.Now()}, UUID: uuid.NewV4().String(), Username: "superUser", Password: "e10adc3949ba59abbe56e057f20f883e", NickName: "普通管理员", HeaderImg: "https://www.mineor.xyz/images/avatar.png", AuthorityId: "1001"},
 }
 
 func (a *admin) Init() error {
